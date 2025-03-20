672d6329469838f7cb2df63932b88808cc26d27d
Eduard B <Eduard.Bartolovic@posteo.de>
2025-03-20 22:14:54 +0100

    Optimized memory usage

--- src/util/Voting.py
+++ src/util/Voting.py
@@ -14,7 +14,6 @@ os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
-os.environ["NUMBA_THREADING_LAYER"] = "tbb"
 
 def print_memory_usage(message=""):
     # TODO Check if required
