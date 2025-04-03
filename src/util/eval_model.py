d952e958dea804b4bb456f44980972751d9ab85b
Eduard <Eduard.Bartolovic@posteo.de>
2025-04-03 09:38:55 +0200

    fix

--- src/util/eval_model.py
+++ src/util/eval_model.py
@@ -128,7 +128,7 @@ def evaluate(device, batch_size, backbone, test_path, distance_metric, test_tran
         metric_concat = {}
     else:
         metric_concat, y_true_concat, y_pred_concat = concat(embedding_library, disable_bar)
-        error_rate_per_class(embedding_library.query_labels, y_pred_top1, os.path.basename(test_path) + '_concat')
+        error_rate_per_class(y_true_concat, y_pred_concat, os.path.basename(test_path) + '_concat')
         plot_confusion_matrix(y_true_concat, y_pred_concat, dataset_enrolled, os.path.basename(test_path) + '_concat', matplotlib=False)
 
     return metrics, metrics_front, metrics_knn_voting, metric_concat, embedding_metrics, embedding_library, dataset_enrolled, dataset_query
