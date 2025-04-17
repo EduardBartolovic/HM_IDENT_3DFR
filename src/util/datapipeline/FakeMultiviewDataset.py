e473dfd682ab942036ab3a465eca43265cba2ad6
Eduard <Eduard.Bartolovic@posteo.de>
2025-04-17 10:47:49 +0200

    Added FakeMultiviewDataset.py to test embedding stability without multiview

---
+++ src/util/datapipeline/FakeMultiviewDataset.py
@@ -0,0 +1,98 @@
+import os
+import re
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from PIL import Image, ImageEnhance
+
+
+class FakeMultiviewDataset(Dataset):
+    def __init__(self, root_dir, num_views, transform=None, use_face_corr=True):
+        """
+        Args:
+            root_dir (string): Path to the root directory of the dataset.
+            num_views (int): Number of views (augmentations of front view) to return.
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.root_dir = root_dir
+        self.transform = transform
+        self.num_views = num_views
+        self.face_cor_exist = False
+        self.use_face_corr = use_face_corr
+        self.class_to_idx = self._get_class_to_idx()
+        self.data = self._load_data()
+        self.classes = self._find_classes()
+
+        # Define consistent brightness factors (or any other parameter)
+        self.augmentation_factors = self._generate_factors(0.7, 1.3, self.num_views)
+
+    def _get_class_to_idx(self):
+        classes = sorted(os.listdir(self.root_dir))
+        return {class_name: idx for idx, class_name in enumerate(classes)}
+
+    def _generate_factors(self, min_val, max_val, n):
+        if n == 1:
+            return [1.0]
+        return [min_val + (max_val - min_val) * i / (n - 1) for i in range(n)]
+
+    def _find_classes(self):
+        class_names = sorted([d for d in os.listdir(self.root_dir) if os.path.isdir(os.path.join(self.root_dir, d))])
+        return {class_name: idx for idx, class_name in enumerate(class_names)}
+
+    def _load_data(self):
+        data = []
+        for class_name in os.listdir(self.root_dir):
+            class_path = os.path.join(self.root_dir, class_name)
+            if os.path.isdir(class_path):
+                class_idx = self.class_to_idx[class_name]
+                for filename in os.listdir(class_path):
+                    if filename.endswith((".jpg", ".png", ".jpeg", ".webp")) and "0_0" in filename:
+                        file_path = os.path.join(class_path, filename)
+                        if os.path.isfile(file_path):
+                            data.append((file_path, class_idx))
+
+                    elif filename.endswith(".npz"):
+                        self.face_cor_exist = True
+
+        if not self.use_face_corr:
+            self.face_cor_exist = False
+
+        return data
+
+    def _apply_fixed_augmentations(self, img, idx):
+        """
+        Apply deterministic augmentations like brightness change based on index.
+        """
+        factor = self.augmentation_factors[idx]
+        enhancer = ImageEnhance.Brightness(img)
+        img = enhancer.enhance(factor)
+        return img
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path, class_idx = self.data[idx]
+
+        # Load the front-view image
+        img = Image.open(img_path).convert("RGB")
+
+        # Prepare augmented views
+        images = []
+        for i in range(self.num_views):
+            aug_img = self._apply_fixed_augmentations(img, i)
+            if self.transform:
+                aug_img = self.transform(aug_img)
+            images.append(aug_img)
+
+        # Duplicate face correspondence if available
+        if self.face_cor_exist:
+            face_corr = np.load(img_path.replace("_image.jpg", "_corr.npz"))['corr']
+            face_corr_tensor = torch.Tensor(np.stack([face_corr] * self.num_views))
+        else:
+            face_corr_tensor = torch.Tensor([])
+
+        perspectives = ["0_0"] * self.num_views
+
+        return images, class_idx, perspectives, face_corr_tensor
