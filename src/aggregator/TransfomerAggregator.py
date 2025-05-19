6ce16a1ea6ccf0b4d95a0babcb2186e6cddde3f6
Eduard <Eduard.Bartolovic@posteo.de>
2025-05-19 09:28:39 +0200

    Small changes

--- src/aggregator/TransfomerAggregator.py
+++ src/aggregator/TransfomerAggregator.py
@@ -80,7 +80,7 @@ def make_stt_aggregator(channels_list):
         if channels == 512:
             aggregators.append(TransfomerAggregator(num_views=26, spatial_size=7, transformer_dim=512, feature_dim=512))
         elif channels == 256:
-            aggregators.append(TransfomerAggregator(num_views=26, spatial_size=14, transformer_dim=256, feature_dim=512))
+            aggregators.append(TransfomerAggregator(num_views=26, spatial_size=14, transformer_dim=256, feature_dim=256))
         else:
             aggregators.append(MeanAggregator())
 
