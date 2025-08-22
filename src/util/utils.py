bf2ab2585a5910cca873a97238856a3032f6ee09
Eduard B <Eduard.Bartolovic@posteo.de>
2025-08-22 15:25:44 +0200

    Refactoring / Cleanup

--- src/util/utils.py
+++ src/util/utils.py
@@ -1,30 +1,7 @@
-import torch
-import torchvision.transforms as transforms
-import numpy as np
-from PIL import Image
-from sklearn import metrics
-
 from .misc import colorstr
-from .verification import evaluate
-
-from datetime import datetime
 import matplotlib.pyplot as plt
 
 plt.switch_backend('agg')
-
-
-# Support: ['get_time', 'l2_norm', 'make_weights_for_balanced_classes', 'get_val_pair', 'get_val_data', 'separate_irse_bn_paras', 'separate_resnet_bn_paras', 'warm_up_lr', 'schedule_lr', 'de_preprocess', 'hflip_batch', 'ccrop_batch', 'gen_plot', 'perform_val', 'buffer_val', 'AverageMeter', 'accuracy']
-
-
-def get_time():
-    return (str(datetime.now())[:-10]).replace(' ', '-').replace(':', '-')
-
-
-def l2_norm(input, axis=1):
-    norm = torch.norm(input, 2, axis, True)
-    output = torch.div(input, norm)
-
-    return output
 
 
 def make_weights_for_balanced_classes(images, nclasses):
@@ -49,16 +26,6 @@ def make_weights_for_balanced_classes(images, nclasses):
         weight[idx] = weight_per_class[val[1]]
 
     return weight
-
-# def get_val_data(data_path):
-#     lfw, lfw_issame = get_val_pair(data_path, 'lfw')
-#     cfp_ff, cfp_ff_issame = get_val_pair(data_path, 'cfp_ff')
-#     cfp_fp, cfp_fp_issame = get_val_pair(data_path, 'cfp_fp')
-#     agedb_30, agedb_30_issame = get_val_pair(data_path, 'agedb_30')
-#     calfw, calfw_issame = get_val_pair(data_path, 'calfw')
-#     cplfw, cplfw_issame = get_val_pair(data_path, 'cplfw')
-#     vgg2_fp, vgg2_fp_issame = get_val_pair(data_path, 'vgg2_fp')
-#     return lfw, cfp_ff, cfp_fp, agedb_30, calfw, cplfw, vgg2_fp, lfw_issame, cfp_ff_issame, cfp_fp_issame, agedb_30_issame, calfw_issame, cplfw_issame, vgg2_fp_issame
 
 
 def separate_irse_bn_paras(modules):
@@ -98,53 +65,12 @@ def warm_up_lr(batch, num_batch_warm_up, init_lr, optimizer):
     for params in optimizer.param_groups:
         params['lr'] = batch * init_lr / num_batch_warm_up
 
-    # print(optimizer)
-
 
 def schedule_lr(optimizer):
     for params in optimizer.param_groups:
         params['lr'] /= 10.
 
     print(colorstr('magenta', optimizer))
-
-
-def de_preprocess(tensor):
-    return tensor * 0.5 + 0.5
-
-
-hflip = transforms.Compose([
-    de_preprocess,
-    transforms.ToPILImage(),
-    transforms.functional.hflip,
-    transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-])
-
-
-def hflip_batch(imgs_tensor):
-    hfliped_imgs = torch.empty_like(imgs_tensor)
-    for i, img_ten in enumerate(imgs_tensor):
-        hfliped_imgs[i] = hflip(img_ten)
-
-    return hfliped_imgs
-
-
-ccrop = transforms.Compose([
-    de_preprocess,
-    transforms.ToPILImage(),
-    transforms.Resize([128, 128]),  # smaller side resized
-    transforms.CenterCrop([112, 112]),
-    transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-])
-
-
-def ccrop_batch(imgs_tensor):
-    ccropped_imgs = torch.empty_like(imgs_tensor)
-    for i, img_ten in enumerate(imgs_tensor):
-        ccropped_imgs[i] = ccrop(img_ten)
-
-    return ccropped_imgs
 
 
 def gen_plot(fpr, tpr):
@@ -157,57 +83,6 @@ def gen_plot(fpr, tpr):
     #plt.show()
     plt.close(fig)
     return fig
-
-
-def perform_val(multi_gpu, device, embedding_size, batch_size, backbone, carray, issame, tta=True):
-    if multi_gpu:
-        backbone = backbone.module  # unpackage model from DataParallel
-        backbone = backbone.to(device)
-    else:
-        backbone = backbone.to(device)
-    backbone.eval()  # switch to evaluation mode
-
-    idx = 0
-    embeddings = np.zeros([len(carray), embedding_size])
-    with torch.no_grad():
-        while idx + batch_size <= len(carray):
-            batch = torch.tensor(carray[idx:idx + batch_size][:, [2, 1, 0], :, :])
-            if tta:
-                ccropped = ccrop_batch(batch)
-                fliped = hflip_batch(ccropped)
-                emb_batch = backbone(ccropped.to(device)).cpu() + backbone(fliped.to(device)).cpu()
-                embeddings[idx:idx + batch_size] = l2_norm(emb_batch)
-            else:
-                ccropped = ccrop_batch(batch)
-                embeddings[idx:idx + batch_size] = l2_norm(backbone(ccropped.to(device))).cpu()
-            idx += batch_size
-        if idx < len(carray):
-            batch = torch.tensor(carray[idx:])
-            if tta:
-                ccropped = ccrop_batch(batch)
-                fliped = hflip_batch(ccropped)
-                emb_batch = backbone(ccropped.to(device)).cpu() + backbone(fliped.to(device)).cpu()
-                embeddings[idx:] = l2_norm(emb_batch)
-            else:
-                ccropped = ccrop_batch(batch)
-                embeddings[idx:] = l2_norm(backbone(ccropped.to(device))).cpu()
-
-    tpr, fpr, accuracy, best_thresholds = evaluate(embeddings, issame, nrof_folds)
-    buf = gen_plot(fpr, tpr)
-    roc_curve = Image.open(buf)
-    roc_curve_tensor = transforms.ToTensor()(roc_curve)
-
-    return accuracy.mean(), best_thresholds.mean(), roc_curve_tensor
-
-
-def buffer_val(writer, db_name, acc, best_threshold, roc_curve_tensor, epoch):
-    writer.add_scalar('{}_Accuracy'.format(db_name), acc, epoch)
-    writer.add_scalar('{}_Best_Threshold'.format(db_name), best_threshold, epoch)
-    writer.add_image('{}_ROC_Curve'.format(db_name), roc_curve_tensor, epoch)
-
-
-def buffer_val_min(writer, db_name, acc, epoch):
-    writer.add_scalar('{}_Accuracy'.format(db_name), acc, epoch)
 
 
 class AverageMeter(object):
@@ -232,7 +107,7 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def accuracy(output, target, topk=(1,)):
+def train_accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
     maxk = max(topk)
     batch_size = target.size(0)
