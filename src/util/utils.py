0524e033677d3aaf983ba420c842c9d2d1d35aa0
Eduard B <Eduard.Bartolovic@posteo.de>
2025-08-30 11:42:17 +0200

    Clean up

--- src/util/utils.py
+++ src/util/utils.py
@@ -48,20 +48,6 @@ def separate_bn_paras(module):
     return paras_only_bn, paras_wo_bn
 
 
-def separate_resnet_bn_paras(modules):
-    all_parameters = modules.parameters()
-    paras_only_bn = []
-
-    for pname, p in modules.named_parameters():
-        if pname.find('bn') >= 0:
-            paras_only_bn.append(p)
-
-    paras_only_bn_id = list(map(id, paras_only_bn))
-    paras_wo_bn = list(filter(lambda p: id(p) not in paras_only_bn_id, all_parameters))
-
-    return paras_only_bn, paras_wo_bn
-
-
 def warm_up_lr(batch, num_batch_warm_up, init_lr, optimizer):
     for params in optimizer.param_groups:
         params['lr'] = batch * init_lr / num_batch_warm_up
