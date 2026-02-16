872099c72af88da07b134dac5f39ad4c106f772d
Eduard B <Eduard.Bartolovic@posteo.de>
2026-02-16 20:16:59 +0100

    Simplified code for video dataset generation

--- src/preprocess_datasets/headPoseEstimation/hpe_to_dataset.py
+++ src/preprocess_datasets/headPoseEstimation/hpe_to_dataset.py
@@ -53,7 +53,7 @@ def generate_voxceleb_dataset_from_video(folder_root, dataset_output_folder, kee
             total_files += 1
 
     for job in tqdm(jobs, desc="Generate Dataset"):
-        success_count, errors = process_txt_file_to_video_voxceleb(job)
+        success_count, errors = process_txt_file_to_video_voxceleb_nersemble(job)
         total_success += success_count
         total_errors += errors
 
@@ -61,7 +61,7 @@ def generate_voxceleb_dataset_from_video(folder_root, dataset_output_folder, kee
     print(f"Copied {total_success} files in {elapsed_time / 60:.2f} min, with {total_errors} errors.")
 
 
-def process_txt_file_to_video_voxceleb(args):
+def process_txt_file_to_video_voxceleb_nersemble(args):
     file_path, destination, hash_name, keep, video_folder_path = args
     data = read_file(file_path)
     errors = 0
@@ -155,70 +155,12 @@ def generate_nersemble_dataset_from_video(folder_root, dataset_output_folder, ke
             total_files += 1
 
     for job in tqdm(jobs, desc="Generate Dataset"):
-        #success_count, errors = process_txt_file_to_video_nersemble(job) # TODO: CHECK
-        success_count, errors = process_txt_file_to_video_voxceleb(job)
+        success_count, errors = process_txt_file_to_video_voxceleb_nersemble(job)
         total_success += success_count
         total_errors += errors
 
     elapsed_time = time.time() - start_time
     print(f"Copied {total_success} files in {elapsed_time / 60:.2f} min, with {total_errors} errors.")
-
-
-def process_txt_file_to_video_nersemble(args):
-    file_path, destination, hash_name, keep, video_folder_path = args
-    data = read_file(file_path)
-    errors = 0
-    success_count = 0
-    video_cache = {}  # Cache for open VideoCapture objects
-
-    for info in data:
-        try:
-            video_name, frame_index = info[7].split('#')
-            frame_index = int(frame_index)
-
-            val = info[12]
-            was_flipped = str(val).lower() in ["true"]
-            flip_tag = "f" if was_flipped else ""
-
-            dst_filename = (
-                f'{hash_name[:15]}#{info[0]}_{info[1]}'
-                f'#{clean_zero(info[3].split(".")[0])}_{clean_zero(info[4].split(".")[0])}'
-                f'{flip_tag}.jpg'
-            )
-            dst_path = os.path.join(destination, dst_filename)
-
-        except ValueError as e:
-            print("Error in :", file_path)
-            raise e
-
-        if keep and os.path.exists(dst_path):
-            continue
-
-        if video_name not in video_cache:
-            video_path = os.path.join(video_folder_path, video_name)
-            cap = cv2.VideoCapture(video_path)
-            video_cache[video_name] = cap
-        else:
-            cap = video_cache[video_name]
-
-        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-        ret, frame = cap.read()
-
-        if not ret:
-            errors += 1
-            continue
-
-        # apply horizontal flip if needed
-        if was_flipped:
-            frame = cv2.flip(frame, 1)
-
-        cv2.imwrite(dst_path, frame)
-        success_count += 1
-
-    for cap in video_cache.values():
-        cap.release()
-
-    return success_count, errors
 
 
 def generate_ytf_dataset_from_video(folder_root, dataset_output_folder, keep=True):
